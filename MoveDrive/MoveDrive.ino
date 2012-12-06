b69b7d1942b1e4efef7e3c1553f5fb81992a751a
thed636 <orionstation@rambler.ru>
2012-12-06 22:43:08 +0400

    New calls dispached

--- MoveDrive/MoveDrive.ino
+++ MoveDrive/MoveDrive.ino
@@ -1,6 +1,8 @@
 #include <pwm_driver.h>
+#include <drives.h>
 #include "D:\workspace\arductl\src\messages.h"
 PwmDriver drive1(22,23,2);
+ServoDrive baseDrive(22,23,2,24);
 
 
 // the setup routine runs once when you press reset:
@@ -40,8 +42,18 @@ void loop() {
         response.result = 0;
       HANDLE_MESSAGE_END;
       HANDLE_MESSAGE_BEGIN(MoveDriveTo,ArduinoResponse)
-        drive1.start(request.speed,request.x>0);
+        baseDrive.setMaxSpeed(request.speed);
+        baseDrive.target(request.x);
         response.result = 0;
+      HANDLE_MESSAGE_END;
+      HANDLE_MESSAGE_BEGIN(GetDrivePosition,DrivePosition)
+        response.pos = baseDrive.position();
+      HANDLE_MESSAGE_END;
+      HANDLE_MESSAGE_BEGIN(GetDriveSpeed,DriveSpeed)
+        response.speed = baseDrive.speed();
+      HANDLE_MESSAGE_END;
+      HANDLE_MESSAGE_BEGIN(GetDrivePositionError,DrivePositionError)
+        response.error = baseDrive.error();
       HANDLE_MESSAGE_END;
       HANDLE_MESSAGE_BEGIN(StopDrive,ArduinoResponse)
         drive1.stop();
@@ -49,34 +61,6 @@ void loop() {
       HANDLE_MESSAGE_END;
     MESSAGE_HANDLERS_END;
   }
+  baseDrive.update();
 }
 
-//// the loop routine runs over and over again forever:
-//void loop() {
-//  if (Serial.available() >= packetSize) {  //если есть доступные данные
-//    // считываем байт
-//    for( int i = 0; i!= packetSize; ++i )
-//      inBuf[i]= Serial.read();
-//      //if( inBuf[0] == 0xF ) {
-//    MESSAGE_HANDLERS_BEGIN
-//      HANDLE_MESSAGE_BEGIN(PwmOut,ArduinoResponse)
-//        PwmPin pin(request.pin);
-//        pin.value(request.value);
-//        response.result = 0;
-//      HANDLE_MESSAGE_END;
-//      HANDLE_MESSAGE_BEGIN(MoveDriveTo,ArduinoResponse)
-//        drive1.start(request.speed,request.x>0);
-//        response.result = 0;
-//      HANDLE_MESSAGE_END;
-//      HANDLE_MESSAGE_BEGIN(StopDrive,ArduinoResponse)
-//        drive1.stop();
-//        response.result = 0;
-//      HANDLE_MESSAGE_END;
-//    MESSAGE_HANDLERS_END;
-//    //ledPin.value(inBuf[1]);
-//    Serial.flush();
-//      //lightOn();
-//    Serial.write((uint8_t*)outBuf, packetSize);
-//      //lightOff();
-//  }
-//}
