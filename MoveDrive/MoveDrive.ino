5899878dd0ce718aeb19c6b3c489a4cb2a867b10
thed636 <orionstation@rambler.ru>
2012-12-06 16:14:43 +0400

    MPI interface created

--- MoveDrive/MoveDrive.ino
+++ MoveDrive/MoveDrive.ino
@@ -1,45 +1,7 @@
 #include <pwm_driver.h>
-
+#include "D:\workspace\arductl\src\messages.h"
 PwmDriver drive1(22,23,2);
 
-
-#pragma pack(1)
-const uint32_t packetSize = 16;
-
-struct ArduinoMessage {
-    uint8_t type;
-    ArduinoMessage( char type ) : type(type) {}
-};
-
-struct PwmOut : public ArduinoMessage {
-    enum {messageType = 0x1, freeSize = packetSize - sizeof(ArduinoMessage) - 3};
-    uint8_t pin;
-    uint8_t value;
-    uint8_t zero;
-    uint8_t dummy[freeSize];
-    PwmOut( uint8_t pin, uint8_t value ) : ArduinoMessage(messageType), pin(pin), value(value), zero(0) {}
-};
-
-struct MoveDriveTo : public ArduinoMessage {
-    enum {messageType = 0x2, freeSize = packetSize - sizeof(ArduinoMessage) - 7};
-    uint8_t drive;
-    uint8_t speed;
-    int32_t x;
-    uint8_t zero;
-    uint8_t dummy[freeSize];
-    MoveDriveTo(uint8_t drive, uint8_t speed, int32_t x)
-    : ArduinoMessage(messageType), drive(drive), speed(speed), x(x), zero(0) {}
-};
-
-struct ArduinoResponse {
-    uint8_t result;
-    ArduinoResponse(uint8_t result = 0) : result(result) {}
-};
-
-#pragma pack()
-
-char inBuf[packetSize];
-uint8_t outBuf[packetSize];
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -47,44 +9,74 @@ void setup() {
   Serial.flush();
   while( Serial.available() )
     Serial.read();
-  
-  for( int i=0; i!=packetSize; ++i ) {
-    inBuf[i]=0x1;
-    outBuf[i]=0x1;
-  }
+
   Serial.write(0x22);
 }
 
+//Here can be just Serial.peek()
+#define MESSAGE_HANDLERS_BEGIN switch( Serial.peek() ) {
+
+#define MESSAGE_HANDLERS_END default:;}
+
+#define HANDLE_MESSAGE_BEGIN(Request,Response)\
+  case Request::messageType : {\
+    typedef Response __Resp; \
+    if(Serial.available()>=sizeof(Request)) { \
+      Request request;\
+      char * inBuf((char*)(&request));\
+      for( int i = 0; i!= sizeof(Request); ++i ) \
+        inBuf[i]= Serial.read(); \
+      Response response;
+         
+#define HANDLE_MESSAGE_END Serial.flush(); Serial.write((uint8_t*)(&response), sizeof(__Resp));}} break
 
 // the loop routine runs over and over again forever:
 void loop() {
-  if (Serial.available() >= packetSize) {  //если есть доступные данные
-    // считываем байт
-    for( int i = 0; i!= packetSize; ++i )
-      inBuf[i]= Serial.read();
-      //if( inBuf[0] == 0xF ) {
-    switch( ((ArduinoMessage*)inBuf)->type ) {
-      case PwmOut::messageType:
-      {
-        const PwmOut & msg(*((PwmOut*)inBuf));
-        PwmPin pin(msg.pin);
-        pin.value(msg.value);
-        ArduinoResponse & response(*((ArduinoResponse*)outBuf));
+  if (Serial.available()) {  //если есть доступные данные
+    MESSAGE_HANDLERS_BEGIN
+      HANDLE_MESSAGE_BEGIN(PwmOut,ArduinoResponse)
+        PwmPin pin(request.pin);
+        pin.value(request.value);
         response.result = 0;
-      }
-      case MoveDriveTo::messageType:
-      {
-        const MoveDriveTo & msg(*((MoveDriveTo*)inBuf));
-        drive1.start(msg.speed,msg.x>0);
-        ArduinoResponse & response(*((ArduinoResponse*)outBuf));
+      HANDLE_MESSAGE_END;
+      HANDLE_MESSAGE_BEGIN(MoveDriveTo,ArduinoResponse)
+        drive1.start(request.speed,request.x>0);
         response.result = 0;
-      }
-      default:;
-    }
-    //ledPin.value(inBuf[1]);
-    Serial.flush();
-      //lightOn();
-    Serial.write((uint8_t*)outBuf, packetSize);
-      //lightOff();
+      HANDLE_MESSAGE_END;
+      HANDLE_MESSAGE_BEGIN(StopDrive,ArduinoResponse)
+        drive1.stop();
+        response.result = 0;
+      HANDLE_MESSAGE_END;
+    MESSAGE_HANDLERS_END;
   }
 }
+
+//// the loop routine runs over and over again forever:
+//void loop() {
+//  if (Serial.available() >= packetSize) {  //если есть доступные данные
+//    // считываем байт
+//    for( int i = 0; i!= packetSize; ++i )
+//      inBuf[i]= Serial.read();
+//      //if( inBuf[0] == 0xF ) {
+//    MESSAGE_HANDLERS_BEGIN
+//      HANDLE_MESSAGE_BEGIN(PwmOut,ArduinoResponse)
+//        PwmPin pin(request.pin);
+//        pin.value(request.value);
+//        response.result = 0;
+//      HANDLE_MESSAGE_END;
+//      HANDLE_MESSAGE_BEGIN(MoveDriveTo,ArduinoResponse)
+//        drive1.start(request.speed,request.x>0);
+//        response.result = 0;
+//      HANDLE_MESSAGE_END;
+//      HANDLE_MESSAGE_BEGIN(StopDrive,ArduinoResponse)
+//        drive1.stop();
+//        response.result = 0;
+//      HANDLE_MESSAGE_END;
+//    MESSAGE_HANDLERS_END;
+//    //ledPin.value(inBuf[1]);
+//    Serial.flush();
+//      //lightOn();
+//    Serial.write((uint8_t*)outBuf, packetSize);
+//      //lightOff();
+//  }
+//}
